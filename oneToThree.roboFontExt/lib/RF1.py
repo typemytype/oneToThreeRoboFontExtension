67f46db03b99d2eb0b74140fe22cb9c0587a9b04
Frederik Berlaen <frederik@typemytype.com>
2018-01-26 12:04:10 +0100

    in RF1 it was addGuide...

--- oneToThree.roboFontExt/lib/RF1.py
+++ oneToThree.roboFontExt/lib/RF1.py
@@ -21,11 +21,11 @@ needIdentifiers = [
 def _getIdentifier(self):
     return self.identifier
 
-def _generateIdentifier(self):        
+def _generateIdentifier(self):
     import uuid
     self.identifier = uuid.uuid4().hex
     return self.identifier
-    
+
 for obj in needIdentifiers:
     obj.identifier = None
     obj.getIdentifier = _getIdentifier
@@ -45,17 +45,17 @@ needGuidelines = [
 
 for obj in needGuidelines:
     obj.guidelines = obj.guides
-    obj.appendGuideline = obj.appendGuide
+    obj.appendGuideline = obj.addGuide
 
 ############################
-# representation factories #    
+# representation factories #
 ############################
 
 import defcon
 
 def _registerRepresentationFactory(cls, name, factory, destructiveNotifications=None):
     def factoryWrapper(glyph, font, *args, **kwargs):
-        return factory(glyph, *args, **kwargs)        
+        return factory(glyph, *args, **kwargs)
     defcon.addRepresentationFactory(name, factoryWrapper)
 
 defcon.registerRepresentationFactory = _registerRepresentationFactory
